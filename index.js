802fe92bfc12b004bf1acf58b4b398026d292aa2
Jerry Velazquez <jvr572@gmail.com>
2019-01-29 22:20:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ function prependKitten(name){
   return [name,...kittens];
 }
 function removeLastKitten(){
-  
+  return kittens.slice(0, kittens.length -1)
 }
-function 
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
